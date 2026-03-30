e1b94c499690da327a320207ad44e69e4a31dbcf
MinHyeong <minhyeong61022@gmail.com>
2026-03-30 15:19:26 +0900

    [Init] 초기화

---
+++ OZ_Coding_CS/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OZ_Coding_CS
+{
+    internal class Program
+    {
+        static int _globalCoffeePrice;
+        static void Main(string[] args)
+        {
+            int coffeePrice = 0;
+            float naftaWeight = 5.5f;
+        }
+    }
+}
