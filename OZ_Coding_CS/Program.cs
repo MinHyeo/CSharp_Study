3e876328c18620257789ca51e740599bae9dbd17
MinHyeong <minhyeong61022@gmail.com>
2026-03-30 16:32:56 +0900

    [update] 계산기 구현
    사칙연산과 나머지를 계산해주는 계산기 구현

--- OZ_Coding_CS/Program.cs
+++ OZ_Coding_CS/Program.cs
@@ -4,13 +4,52 @@ namespace OZ_Coding_CS
 {
     internal class Program
     {
-        static int _globalCoffeePrice;
+        float InputNumber()
+        {
+            Console.Write("숫자를 입력해주세요 : ");
+            return float.Parse(Console.ReadLine());
+        }
+
+        char InputOperator()
+        {
+            Console.Write("연산을 입력해주세요( +, -, *, /, % ) : ");
+            return char.Parse(Console.ReadLine());
+        }
+
+        float Calculate(float inputA, float inputB, char operatorChar)
+        {
+            switch (operatorChar)
+            {
+                case '+':
+                    return inputA + inputB;
+                case '-':
+                    return inputA - inputB;
+                case '*':
+                    return inputA * inputB;
+                case '/':
+                    return inputA / inputB;
+                case '%':
+                    return inputA % inputB;
+            }
+            return 0;
+        }
+
         static void Main(string[] args)
         {
-            int coffeePrice = 0;
-            float naftaWeight = 5.5f;
+            Program program = new Program();
 
-            //dddd
+            // 입력 값
+            float inputA, inputB;
+            char operatorChar;
+
+            // 수 입력
+            inputA = program.InputNumber();
+            inputB = program.InputNumber();
+
+            // 연산 종류 입력
+            operatorChar = program.InputOperator();
+
+            Console.WriteLine($"연산 결과 : {program.Calculate(inputA, inputB, operatorChar)}");
         }
     }
 }
