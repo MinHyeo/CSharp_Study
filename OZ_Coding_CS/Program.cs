ea55a977400b965602cb56127dc840350e42620f
MinHyeong <minhyeong61022@gmail.com>
2026-03-31 17:13:21 +0900

    [update] Class 연습
    손님과 카페직원으로 객체 생성 및 상호작용 연습

--- OZ_Coding_CS/Program.cs
+++ OZ_Coding_CS/Program.cs
@@ -1,55 +1,80 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace OZ_Coding_CS
 {
+    class CafeStaff
+    {
+        private int _money;
+        private int _coffeeBeanCount;
+
+        public CafeStaff(int money, int coffeeBeanCount)
+        {
+            _money = money;
+            _coffeeBeanCount = coffeeBeanCount;
+
+            Console.WriteLine("카페 직원 출근!");
+        }
+
+        public void MakeCoffee(int money)
+        {
+            Console.WriteLine($"커피 가격은 {money}원 입니다.");
+            _money += money;
+            _coffeeBeanCount -= 1;
+        }
+
+        public void CallCustomer(Customer customer)
+        {
+            Console.WriteLine($"{customer._id}번 손님. 주문하신 커피 나왔습니다.");
+            GiveCoffee(customer);
+        }
+
+        private void GiveCoffee(Customer customer)
+        {
+            customer.DrinkCoffee(10);
+        }
+    }
+
+    class Customer
+    {
+        public int _id;
+        private int _money;
+        private int _fatigability;
+
+        public Customer(int id, int money, int fatigability)
+        {
+            _id = id;
+            _money = money;
+            _fatigability = fatigability;
+
+            Console.WriteLine("손님 입장");
+        }
+
+        public void OrderCoffee(CafeStaff staff, int coffePrice)
+        {
+            Console.WriteLine("커피 한잔 주세요!");
+
+            _money = _money - coffePrice;
+            staff.MakeCoffee(coffePrice);
+        }
+
+        public void DrinkCoffee(int fatigability)
+        {
+            _fatigability -= fatigability;
+            Console.WriteLine($"커피를 마셔 피로도가 10 줄었습니다. 남은 피로도 : {_fatigability}");
+        }
+    }
+
     internal class Program
     {
-        float InputNumber()
-        {
-            Console.Write("숫자를 입력해주세요 : ");
-            return float.Parse(Console.ReadLine());
-        }
-
-        char InputOperator()
-        {
-            Console.Write("연산을 입력해주세요( +, -, *, /, % ) : ");
-            return char.Parse(Console.ReadLine());
-        }
-
-        float Calculate(float inputA, float inputB, char operatorChar)
-        {
-            switch (operatorChar)
-            {
-                case '+':
-                    return inputA + inputB;
-                case '-':
-                    return inputA - inputB;
-                case '*':
-                    return inputA * inputB;
-                case '/':
-                    return inputA / inputB;
-                case '%':
-                    return inputA % inputB;
-            }
-            return 0;
-        }
-
         static void Main(string[] args)
         {
-            Program program = new Program();
+            CafeStaff cafeStaff = new CafeStaff(0, 10);
+            Customer customer = new Customer(1, 5000, 100);
 
-            // 입력 값
-            float inputA, inputB;
-            char operatorChar;
+            customer.OrderCoffee(cafeStaff, 1000);
 
-            // 수 입력
-            inputA = program.InputNumber();
-            inputB = program.InputNumber();
-
-            // 연산 종류 입력
-            operatorChar = program.InputOperator();
-
-            Console.WriteLine($"연산 결과 : {program.Calculate(inputA, inputB, operatorChar)}");
+            cafeStaff.CallCustomer(customer);
         }
     }
 }
