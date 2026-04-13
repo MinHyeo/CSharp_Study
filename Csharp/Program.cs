d6b918ae74487e9d4c14a463fca4166180d677cd
MinHyeong <minhyeong61022@gmail.com>
2026-04-13 17:24:44 +0900

    [update] Interface 공부 목적 Item class 구현
    Interface를 공부하기 위해 IItem을 Item class에 상속하여 사용

--- Csharp/Program.cs
+++ Csharp/Program.cs
@@ -1,18 +1,123 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Security.Cryptography;
 
 namespace CSharp_Study
 {
+    interface IItem
+    {
+        void Use();
+        void Drop();
+        void PrintDescription();
+    }
+
+    public class Item : IItem
+    {
+        protected string _name;
+        protected string _description;
+
+        public Item(string name, string description)
+        {
+            _name = name;
+            _description = description;
+        }
+
+        public virtual void Use()
+        {
+            Console.WriteLine($"{_name}을 사용하였습니다.");
+        }
+
+        public virtual void Drop()
+        {
+            Console.WriteLine($"{_name}을 버렸습니다.");
+        }
+
+        public virtual void PrintDescription()
+        {
+            Console.WriteLine($"- {_name}");
+            Console.WriteLine($"{_description}");
+        }
+    }
+
+    public class Potion : Item
+    {
+        public Potion(string name, string description) : base(name, description)
+        {
+        }
+    }
+
+    public class Weapon : Item
+    {
+        public Weapon(string name, string description) : base(name, description)
+        {
+        }
+
+        public void Repair()
+        {
+            Console.WriteLine($"{_name}를 수리하였습니다.");
+        }
+    }
+
+    public class QuestItem : Item
+    {
+        public QuestItem(string name, string description) : base(name, description)
+        {
+        }
+    }
+
+    public class Inventory
+    {
+        private List<IItem> _items;
+
+        public Inventory()
+        {
+            _items = new List<IItem>();
+
+            _items.Add(new Potion("포션", "체력과 마나를 회복하는 아이템입니다."));
+            _items.Add(new Weapon("무기", "플레이어가 착용할 수 있는 장비입니다."));
+            _items.Add(new QuestItem("퀘스트용 아이템", "퀘스트에서만 사용하는 아이템입니다."));
+        }
+
+        public void UseAllItem()
+        {
+            foreach(var item in _items)
+            {
+                item.PrintDescription();
+                item.Use();
+
+                if(item is Weapon)
+                {
+                    var weapon = item as Weapon;
+                    weapon.Repair();
+                }
+            }
+        }
+
+        public void DropAllItem()
+        {
+            List<IItem> dropItems = new List<IItem>();
+
+            foreach (var item in _items)
+            {
+                item.Drop();
+                dropItems.Add(item);
+            }
+
+            foreach(var item in dropItems)
+            {
+                _items.Remove(item);
+            }
+        }
+    }
+
     class Algorithm
     {
         internal class Program
         {
             static void Main(string[] args)
             {
-
+                Inventory inventory = new Inventory();
+                inventory.UseAllItem();
+                inventory.DropAllItem();
             }
         }
     }
